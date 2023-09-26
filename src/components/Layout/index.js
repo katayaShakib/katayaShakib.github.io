20c004ff00d0ae2f356345c628d595f047b6ddb3
Shakib KATAYA <kataya.shakib@gmail.com>
2023-09-26 16:53:47 +0300

    Render Siderbar comp. inside Layout comp.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,10 @@
+import Sidebar from '../Sidebar'
 import './index.scss'
 
-const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
-}
+const Layout = () => (
+  <div>
+    <Sidebar></Sidebar>
+  </div>
+)
 
 export default Layout
