a0b9ffa0cb55792cd3306e3fde134d66a68c93c9
Shakib KATAYA <kataya.shakib@gmail.com>
2023-09-26 17:59:47 +0300

    Add social links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
@@ -30,6 +30,34 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/shakib-kataya/"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            color="#4d4d4e"
+            className="anchor-icon"
+          ></FontAwesomeIcon>
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/katayaShakib"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            color="#4d4d4e"
+            className="anchor-icon"
+          ></FontAwesomeIcon>
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
