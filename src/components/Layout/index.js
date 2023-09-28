cef4da705876732bd79f54eb200e8ae64e2f9d00
Shakib KATAYA <kataya.shakib@gmail.com>
2023-09-28 17:29:25 +0300

    Create Home comp. & Add some style

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,23 @@
 import Sidebar from '../Sidebar'
 import './index.scss'
+import { Outlet } from "react-router-dom";
 
-const Layout = () => (
-  <div>
-    <Sidebar></Sidebar>
-  </div>
-)
+const Layout = () => {
+  return (
+    <div className="App">
+      <Sidebar></Sidebar>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
 
+        <Outlet></Outlet>
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
+  )
+}
 export default Layout
